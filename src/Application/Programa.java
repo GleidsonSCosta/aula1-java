f6c0561a6322294c63d0b2fde0b086600b2eec3d
Gleidson Costa <gleidsoncosta92@hotmail.com>
2021-10-09 20:18:27 -0300

    Condicionais

--- src/Application/Programa.java
+++ src/Application/Programa.java
@@ -1,10 +1,22 @@
 package Application;
 
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
-		System.out.println("Boa noite");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Quantas horas?");
+		int horas = sc.nextInt();
+		if (horas < 12) {
+			System.out.println("Bom dia!");
+		} else if (horas >= 12 && horas < 18) {
+			System.out.println("Boa tarde!");
+		} else {
+			System.out.println("Boa noite!");
+		}
+
+		sc.close();
 
 	}
-
 }
